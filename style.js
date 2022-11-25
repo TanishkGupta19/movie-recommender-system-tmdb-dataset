2b05b8c96ee953abfba8ac8afcc88c62805e376d
TanishkGupta19 <85075310+TanishkGupta19@users.noreply.github.com>
2022-11-26 00:51:23 +0530

    Add files via upload
    Movie Recommended System

---
+++ style.js
@@ -0,0 +1,20 @@
+alert("Hello Friends, its here Tanishk Gupta");
+
+// Variable ::  javascript is typeless and loosely types
+// JavaScript is not only the Client site programming language.
+var a=10;
+a="Tanishk_";
+var b=20;
+b="Guptaji";
+var c=a+b;
+console.log(c);             // these are the two way by which we can print/display the result
+document.write(c);
+
+//Note ::  We can execute javascript code in the console of the browser
+// Every browser has inbuild engine that is responsible for executing our javascript code
+// inbuild engine :: Promt V8  
+
+document.getElementById("head").innerText = c;         // example
+
+document.getElementsById("head").style.display = "none";
+
